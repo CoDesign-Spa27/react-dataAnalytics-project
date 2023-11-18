789592a10afd6241d99940cac654573dc3506abf
2sandeep7 <ssandeep8696@gmail.com>
2023-11-18 14:01:06 -0500

    added Analytics Named component

--- app/components/Analytics.jsx
+++ app/components/Analytics.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Analytic from '../../public/test-img.jpeg';
 
 const Analytics = () => {
 
@@ -7,8 +7,15 @@ const Analytics = () => {
   return (
     <div className='w-full bg-white py-14 px-4'>
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-   
+      <img src={Analytic}  alt='img'/>
+        <div className='flex flex-col justify-center '>
+      <p className='text-[#00df9a] font-bold border-b text-[1.3rem] md:text-xl'>Data Analytics Dashboard</p>
+          <h1 className='font-bold py-1 text-[1.6rem] md:text-2xl'>Manage Data Analytics Centrally</h1>
+          <p className=' text-sm sm:text-m md:text-l'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, tempore! Aliquam animi unde ipsam maxime sint corporis sapiente adipisci, facere eaque magnam vel debitis est doloremque ullam, odit, consequatur saepe?</p>
+          <button className='text-[#00df9a] bg-[#000300] w-[200px] rounded-md font-medium my-4 mx-auto md:mx-[0] transition duration-700 py-3 hover:bg-[#1dad80] hover:text-white '>Get Started</button>
         </div>
+        </div>
+
       
     </div>
   )
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     {!nav ? <AiOutlineClose size={23}/> : <AiOutlineMenu size={23} />}
    </div>
    <div className={!nav ?'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 ease-in-out duration-500 bg-[#000300]':'fixed left-[-100%]'}>
-   <h1 className='w-full text-3xl font-bold text-[#00df9a] m-3'>React.</h1>
+   <h1 className='w-full text-3xl font-bold text-[#00df9a] m-[20px]'>React.</h1>
    <ul className='uppercase ' >   
       <li className='p-3 border-b border-gray-600'>Home</li>
       <li className='p-3 border-b border-gray-600'>contact</li>
